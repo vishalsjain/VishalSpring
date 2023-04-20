e792e525fd2eddcae67f20add288e9755536aeaa
vishalsjain <vishalsjain89@gmail.com>
2023-04-20 10:36:19 +0530

    sdfsdf

---
+++ src/main/java/com/example/VishalSpring/VishalSpringApplication.java
@@ -0,0 +1,15 @@
+package com.example.VishalSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VishalSpringApplication {
+
+	public static void main(String[] args) {
+		System.out.println("VISHAL JAIN");
+
+		SpringApplication.run(VishalSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/VishalSpring/VishalSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VishalSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VishalSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
